10774337754ddf8886df130fd6315fc9a74f869b
David <david.edeni@icloud.com>
2026-03-14 21:20:47 -0500

    feat: Add main.jsx as the application entry point and append new Firebase debug log entries.

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,13 @@ import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import theme from './theme'
 
-console.log('Main.jsx started - MUI IMPORTS');
-alert('Main.jsx alert - MUI IMPORTS');
-document.body.innerHTML = '<h1>IF YOU SEE THIS, JS IS WORKING - MUI IMPORTS</h1>';
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+)
